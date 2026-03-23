eaa493d8698d2d3f21e2afeb7107f21f91305fd1
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-03-23 15:43:06 +0530

    Added different coins and stop when waterlevel increases above red.

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,8 +1,26 @@
 extends RigidBody2D
 
-signal clicked
+signal clicked(coin_node) 
+
+var water_increase: float = 0.0
+
+func setup(coin_data: Dictionary) -> void:
+	mass = coin_data["weight"]
+	scale = coin_data["scale"]
+	var phys_mat = PhysicsMaterial.new() # this thing is used for adding physics properties for a material
+	phys_mat.bounce = coin_data["bounce"]
+	phys_mat.friction = coin_data["friction"]
+	physics_material_override = phys_mat
+	
+	water_increase = coin_data["water_increase"]
+	
+	$Sprite2D.modulate = coin_data["color"]
+
+func wake_up() -> void:
+	sleeping = false
 
 func _input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		emit_signal("clicked")
+		emit_signal("clicked", self) 
+		get_tree().call_group("Coins", "wake_up")
 		queue_free()
